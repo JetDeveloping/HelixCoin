8f34886df20f4a98f5cdbb00ff5c9d52b628665a
Jet <jetjadeja@gmail.com>
2020-10-26 10:15:32 -0700

    Created Merkle Root Hashing Algo

--- helixcoin/block.py
+++ helixcoin/block.py
@@ -14,3 +14,25 @@ class Block:
     previous_hash: str
     nonce: int
 
+
+    def hash(self, data:list=None):  # Create 
+        if data == None:
+            data = self.transactions
+            
+        result = []
+        for i,k in zip(data[0::2], data[1::2]):
+            try:
+                result_i = i.hash()
+                result_k = k.hash()
+            except:
+                result_i = sha256(str(i).encode()).hexdigest()
+                result_k = sha256(str(k).encode()).hexdigest()
+
+            result.append(sha256(f'{result_i}{result_k}'.encode()).hexdigest())
+
+        if len(result) > 1:
+            result = self.hash(result)
+
+        return result
+        
+
